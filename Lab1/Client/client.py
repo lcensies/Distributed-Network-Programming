d1bff067fbf6a478324b7337be2cb3cf4f9a8a75
username <address@mailserver.com>
2022-09-07 16:42:20 -0400

    Lab 1 - additional fixes

--- Lab1/Client/client.py
+++ Lab1/Client/client.py
@@ -1,14 +1,12 @@
 #! /usr/bin/env python3
-import argparse
 import os
 import socket
 import select
 import sys
 import traceback
-from math import ceil
-from tenacity import retry, stop_after_attempt, retry_if_exception_type, retry_if_result, RetryError
+from tenacity import retry, stop_after_attempt, retry_if_result, RetryError, wait_fixed
 
-debug = True
+debug = False
 usage = f"Usage: python3 server_ip:port src_filename dst_filename"
 
 # client_addr = ('', 9000)
@@ -16,47 +14,30 @@ client_addr = ('localhost', 9000)
 
 client_bufsize = 4096
 start_seqno = 0
-start_seqno = 0
 max_retries = 5
-retry_timeout = 500  # in seconds
+retry_timeout = 3  # in seconds
 
 if len(sys.argv) > 1:
     server_addr = sys.argv[1].split(":")
     server_addr = (server_addr[0], int(server_addr[1]))
 else:
-    server_addr = ('localhost', 3434)
-    # print(usage)
-    # exit()
+    # server_addr = ('localhost', 3434)
+    print(usage)
+    exit()
 
 if len(sys.argv) > 2:
     src_filename = sys.argv[2]
 else:
-    src_filename = "Client/Files/enum.txt"
-    # print(usage)
-    # exit()
+    # src_filename = "Client/Files/enum.txt"
+    print(usage)
+    exit()
 
 if len(sys.argv) > 3:
     dst_filename = sys.argv[3]
 else:
-    dst_filename = "enum.txt"
-    # print(usage)
-    # exit()
-
-
-# server_addr = sys.argv[1].split(":")[0], int(sys.argv[1].split(":")[1]) if len(sys.argv) > 1 else ('localhost', 3434)
-# src_filename = sys.argv[2]
-# dst_filename = sys.argv[3] if len(sys.argv) > 4 else src_filename
-
-# src_filename = "Client/photo.jpg"
-# src_filename = "Client/enum.txt"
-
-# server-hostname:12300 \
-#     path/to/local/file.jpg \
-#     filename-on-server.jpg
-
-
-# script_dir = os.path.abspath(os.path.dirname(__file__))
-# files_dir = os.path.join(script_dir, "Files")
+    # dst_filename = "enum.txt"
+    print(usage)
+    exit()
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.setblocking(0)
@@ -76,21 +57,19 @@ def parse_init_ack(datagram, params=None):
 
     if not (splitted[2].isdigit() and int(splitted[2]) in range(1, 65527)):
         raise Exception(f"Invalid buffer size. Expected integer in range [1, 65527]. Got {splitted[2]}")
-
+    
+    print(f"Buffer size selected by the server: {splitted[2]}")
+    
     return {
         "type": "ack",
         "next_seqno": int(splitted[1]),
         "buf_size": int(splitted[2])
     }
-
-
 def parse_data_ack(datagram, params):
-    
     splitted = datagram.split(" | ")
 
     if not len(splitted) == 2:
         raise Exception("Wrong number of parameters. Expected 2")
-
     if not (splitted[1].isdigit()):
         raise TypeError(f"Invalid sequence number type. Expected integer.")
     
@@ -105,63 +84,19 @@ def parse_data_ack(datagram, params):
         "next_seqno": next_seqno
     }
 
-@retry(stop=stop_after_attempt(max_retries), retry=retry_if_result(lambda x: x == None), reraise=True)
+@retry(stop=stop_after_attempt(max_retries), retry=retry_if_result(lambda x: x is None), wait=wait_fixed(0.15), reraise=True)
 def send_message(message, callback, params=None):
+
     client.sendto(message, server_addr)
     ready = select.select([client], [], [], retry_timeout)
+    if not ready[0]:
+        return None
 
-    for i in range(max_retries):
-        if not ready[0]:
-            continue
+    data = client.recv(client_bufsize).decode("ascii")
+    if data == "":
+        return None
 
-        data = client.recv(client_bufsize).decode("ascii")
-        if not data:
-            return None
-
-        return callback(data, params)
-
-    return None
-
-# @retry(stop=stop_after_attempt(max_retries), retry=retry_if_result(lambda x: x == None), reraise=True)
-# def send_start(dst_filename, total_size):
-#     message = f"s | {start_seqno} | {dst_filename} | {total_size}".encode()
-#     client.sendto(message, server_addr)
-#     # response = client.recv(4096).decode('ascii')
-#     ready = select.select([client], [], [], retry_timeout)
-# 
-#     for i in range(max_retries):
-#         if not ready[0]:
-#             continue
-# 
-#         data = client.recv(client_bufsize).decode()
-#         if not data:
-#             return None
-# 
-#         return parse_init_ack(data)
-# 
-#     return None
-# 
-# @retry(stop=stop_after_attempt(max_retries), retry=retry_if_result(lambda x: x == None), reraise=True)
-# def send_chunk(seq_no, data_bytes):
-#     message = f"d | {seq_no} | ".encode() + data_bytes
-#     
-#     # message = f"d | {seq_no} | {data_bytes}".encode()
-#     client.sendto(message, server_addr)
-#     ready = select.select([client], [], [], retry_timeout)
-#     
-#     for i in range(max_retries):
-#         if not ready[0]:
-#             continue
-# 
-#         data = client.recv(client_bufsize).decode()
-#         if not data:
-#             return None
-#         
-#         return parse_data_ack(data, params={"seq_no": seq_no})
-#     
-#     return None
-
-
+    return callback(data, params)
 def send_file(src_filename, dst_filename):
     # client.sendto(message, server_addr) 
     
@@ -177,16 +112,9 @@ def send_file(src_filename, dst_filename):
         message = f"s | {seq_no} | {dst_filename} | {total_size}".encode("ascii")
         ack = send_message(message, parse_init_ack)
         seq_no = ack["next_seqno"]
-        # ack = send_start(dst_filename, total_size)
-        # next_seqno = ack["next_seqno"]
         server_bufsize = ack["buf_size"]
-
         message_header = f"d | {seq_no} | "
         message_data_size = server_bufsize - len(message_header)
-        # message_data = f.read(message_data_size)
-        
-        
-        # chunks = [None] * ceil(total_size / server_bufsize)
         
         while (data := f.read(message_data_size)):
             message = message_header.encode("ascii") + data
@@ -195,14 +123,6 @@ def send_file(src_filename, dst_filename):
             seq_no = ack["next_seqno"]
             message_header = f"d | {seq_no} | "
             message_data_size = server_bufsize - len(message_header)
-        # Do stuff with byte.
-        
-        # for i in range(len(chunks)):
-        #     chunks[i] = f.read(server_bufsize)
-        #     seq_no = ack["next_seqno"]
-            # message = f"d | {seq_no} | {chunks[i]}"
-            # ack = send_message(message, parse_data_ack, params={"seq_no": seq_no})
-            # ack = send_chunk(seq_no, chunks[i])
         
         if ack is not None:
             print(f"Successfully sent file {src_filename} to the server")
@@ -213,44 +133,10 @@ def send_file(src_filename, dst_filename):
         print(e)
         if debug:
             traceback.print_exc()
-            # client.sendto(message, server_addr)
-            # # response = client.recv(4096).decode('ascii')
-            # ready = select.select([client], [], [], retry_timeout)
 
-# src_filename = "Client/test_file.txt"
-# src_filename = "Client/photo.jpg"
-
-
-
-
-
-
-
+print(f"Trying to send {src_filename} to {server_addr}")
 send_file(src_filename, dst_filename)
 
-# s | seqno 0 | filename | total_size
-
-# Is data handling correct?
-# Improvements: decorators, parallel sending
 # https://stackoverflow.com/questions/52599656/how-to-send-files-in-chunks-by-socket
 # https://stackoverflow.com/questions/2719017/how-to-set-timeout-on-pythons-socket-recv-method
 
-# Retry exception https://tenacity.readthedocs.io/en/latest/
-
-# TODO if ack == None ack = send_start(dst_filename, total_size)
-
-# TODO wipe session 
-
-# TODO handle wrong chunk size
-
-# TODO handle session hijacking
-
-# TODO print bufsize selcted by the server
-
-# TODO print if seq_no failed to send data
-
-# TODO don't override old chunks and print message if duplicate was detected
-
-# TODO send only serv_buf bytes
-
-# TODO error if server was not found
--- Lab1/Server/server.py
+++ Lab1/Server/server.py
@@ -2,100 +2,85 @@ import os
 import random
 import socketserver
 import string
+import sys
 import threading
 import traceback
-from math import ceil
+from pathvalidate import sanitize_filename
 
-from pathvalidate import sanitize_filepath, sanitize_filename
-
-debug = True
-permanent_sessions = True
+debug = False
+permanent_sessions = False
 
 # server_address = ("", 3434)
-server_address = ("127.0.0.1", 3434)
-server_secret = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
+if len(sys.argv) > 1:
+    server_addr = sys.argv[1].split(":")
+    server_addr = (server_addr[0], int(server_addr[1]))
+else:
+    server_address = ("localhost", 3434)
+    
+# Server secret will be used for session identifiers generation
+server_secret = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
 
-# recv_chunk_size = 32
-# buf_size = recv_chunk_size * 2
-
+# By default, max buf_size for the ThreadingUDPServer is 8192
 buf_size = 1024
 
 
 script_dir = os.path.abspath(os.path.dirname(__file__))
-files_dir = os.path.join(script_dir, "Files")
-# r = redis.Redis(host='localhost', port=9999)
-# r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
-# print(r.get("Bahamas"))
+# files_dir = os.path.join(script_dir, "Files")
+files_dir = script_dir
 
 session_storage = {}
 
-
 def parse_message(datagram):
-    
     message = {}
 
     if chr(datagram[0]) == "s":
-        
         # Separate message on 3 groups maximum
         splitted = datagram.split(b" | ", 3)
-        
+
         message["type"] = "start"
-        
         seq_no = splitted[1].decode("ascii")
-        
+
         if not (seq_no.isdigit() and int(seq_no) >= 0):
             raise ValueError(f"Invalid sequence number {splitted[1]}")
 
-
         message["seq_no"] = int(seq_no)
-        
+
         # In order to prevent directory traversing 
         message["filename"] = sanitize_filename(splitted[2].decode("ascii"))
 
         if not splitted[3].isdigit():
             raise ValueError(f"Invalid format of total size {splitted[2]}. Expected integer.")
 
-
         message["total_size"] = int(splitted[3])
 
     elif chr(datagram[0]) == "d":
-
         # Separate message on 2 groups maximum
         splitted = datagram.split(b" | ", 2)
-        
         message["type"] = "data"
-        
         seq_no = splitted[1].decode("ascii")
-        
+
         if not (seq_no.isdigit() and int(seq_no) >= 0):
             raise ValueError(f"Invalid sequence number {seq_no}")
 
         message["seq_no"] = int(seq_no)
-        
         message["data_bytes"] = splitted[2]
     else:
         raise Exception('Unsupported message type')
 
-
     return message
 
 
-# filename | total_size
-
-class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
-    
+class Server(socketserver.DatagramRequestHandler):
     def remove_session(self, sid):
         if permanent_sessions:
             return
         if sid not in session_storage:
-            return 
-        print(f"Client with sid {sid} is inactive for {session_storage[sid]['timer'].interval} seconds. Removing session.")
+            return
+        print(
+            f"Client with sid {sid} is inactive for {session_storage[sid]['timer'].interval} seconds. Removing session.")
         return session_storage.pop(sid, None)
-        
-        # session_storage[sid] = None
-    
+
     def restore_session(self, sid):
-        
         if sid in session_storage:
             return session_storage[sid]
         else:
@@ -108,105 +93,89 @@ class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
         session_storage[sid] = {
             "seq_no": message["seq_no"],
             'start_seqno': message["seq_no"],
-            # "last_seqno": message["seq_no"] + ceil(int(message["total_size"]) / buf_size),
             "filename": message["filename"],
             "bytes_received": 0,
             "total_size": message["total_size"],
-            # "n_chunks":  ceil(int(message["total_size"]) / buf_size),
             "chunks": []
-            # "chunks": [None] * ceil(int(message["total_size"]) / buf_size)
         }
 
         return session_storage[sid]
-    
+
     def save_file(self, sid):
         bytes = b''.join(session_storage[sid]["chunks"])
-        
         filepath = os.path.join(files_dir, session_storage[sid]["filename"])
-        
+
         with open(filepath, "wb") as f:
             f.write(bytes)
-        
+
         print(f"Saved file {session_storage[sid]['filename']}")
     
+    # Not optimal way to obtain current session identifier
     def get_sid(self):
         return self.client_address[0] + str(self.client_address[1]) + server_secret
-    
+
     def handle_start(self, message, sid):
+        # Start new session
         session = self.init_session(message)
         session_storage[sid] = session
 
         print(f'Received start message from {self.client_address}: {message}')
-        
+
         message = f'a | {session["seq_no"] + 1} | {buf_size}'
-        
+
         self.wfile.write(message.encode())
-        
+
         # Remove session if client is inactive for 3 seconds
         session_storage[sid]["timer"] = threading.Timer(3.0, self.remove_session, args=[sid])
         session_storage[sid]["timer"].start()
-        
+
         print(f'Sent start ack message {message}')
-        
-        
+
     def handle_data(self, message, sid):
         if sid not in session_storage:
             raise Exception("Data message for inexistent sid was sent.")
-        
+
         session_storage[sid]["timer"].cancel()
-        
+
         seq_no = message["seq_no"]
 
         start_seqno = session_storage[sid]['start_seqno']
-        
+
         if seq_no <= start_seqno:
             raise Exception(f"Invalid sequence number {seq_no}")
-        
+
         session_storage[sid]["seq_no"] = seq_no
 
-        # chunk_ind = seq_no - session_storage[sid]["start_seqno"] - 1
         chunk_ind = seq_no - session_storage[sid]["start_seqno"] - 1
-        # if seq_no > 0 and session_storage[sid]["chunks"][chunk_ind] is None:
-
         chunks_received = len(session_storage[sid]["chunks"])
         
+        # Appending new file chunk to the array. 
+        # Not optimal way to handle files, especially if file size is large.
         if chunks_received == chunk_ind:
-            # session_storage[sid]["chunks"][chunk_ind] = message["data_bytes"]
             session_storage[sid]["chunks"].append(message["data_bytes"])
             session_storage[sid]["bytes_received"] += len(message["data_bytes"])
         else:
             print(f"Duplicate chunk with sequence number {seq_no} is received")
-        
+
         message = f'a | {seq_no + 1}'
         self.wfile.write(message.encode())
+
         print(f"Sent data ack message {message}")
-        
-        if seq_no == 120:
-            watafak = 3
-        
+
         if session_storage[sid]["bytes_received"] == session_storage[sid]["total_size"]:
-        # if seq_no == session_storage[sid]["last_seqno"]:
             self.save_file(sid)
             session_storage[sid]["timer"] = threading.Timer(1.0, self.remove_session, args=[sid])
             session_storage[sid]["timer"].start()
         elif not permanent_sessions:
             session_storage[sid]["timer"] = threading.Timer(3.0, self.remove_session, args=[sid])
             session_storage[sid]["timer"].start()
-            
 
     def handle(self):
+        # print("Recieved one request from {}".format(self.client_address))
 
-        print("Recieved one request from {}".format(self.client_address))
-        
-        # bytes = self.rfile.readline()
-        
-        # If client will send more data than buf_size, server would read only buf_size
-        bytes = self.rfile.read()
-        datagram = bytes
-        # datagram = bytes.strip()
-        print(f"Datagram Recieved from client is: {datagram}")
-        if "127" in str(datagram):
-            hueta = 4
+        datagram = self.rfile.read(buf_size)
+        # print(f"Datagram Recieved from client is: {datagram}")
+
         try:
             message = parse_message(datagram)
         except Exception as e:
@@ -224,64 +193,18 @@ class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
             print(e)
             if debug:
                 traceback.print_exc()
-            
+
             if self.get_sid() not in session_storage:
                 return
-            
+
             session_storage[self.get_sid()]["timer"] = threading.Timer(3.0, self.remove_session, args=[self.get_sid()])
             session_storage[self.get_sid()]["timer"].start()
 
-            return
-
-        # print(datagram)
-
-        # print("Thread Name:{}".format(threading.current_thread().name))
-
-        # self.wfile.write("Message from Server! Hello Client".encode())
-
-
-server = socketserver.ThreadingUDPServer(server_address, MyUDPRequestHandler)
+server = socketserver.ThreadingUDPServer(server_address, Server)
 print(f"Server is listening on {server_address}")
 server.serve_forever()
 
-
-
-
-# no session, start -> initial messagee
-# no session, data -> malicious client, ignore
-# session, data -> filetransfer
-# session, start -> malfunctioning client / ack message was lost -> reinitialize session
-
-
-# Note that data chunks are stored and assembled in the memory
-
-# Timers https://docs.python.org/3/library/threading.html#timer-objects
-
-# TODO check for data validity
-# TODO duplicates handling
-
-# TODO directory traversal avoidance
-
-# TODO some encoding for bytes?
-
-# TODO split only by fist 2 dashes
-
 # TODO read only specified size
-
-# TODO at last seq_no check if all data is present
-
-# TODO fix duplicates correct handling
-
 # TODO avoid resaving file if last chunk duplicate was received
+# TODO handle incorrect chunk size
 
-# TODO handle if sid not in dict
-
-# TODO save files in server dir
-
-# TODO fix first chunk indexing
-
-# Split by first N ocurrences https://stackoverflow.com/questions/6903557/splitting-on-first-occurrence
-# Last - 894 bytes
-
-
-# Problem: client sends 1024 bytes in total, not only for message.
